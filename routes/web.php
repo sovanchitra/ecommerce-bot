e4d4e4faf1b214172dd4360d7237cdf5b7ccc00f
chivsovanchetra <chivsovanchetra@gmail.com>
2025-04-08 15:35:17 +0700

    add: migrate schema

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\BotController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('telegram/webhook', [BotController::class, 'handle']);
